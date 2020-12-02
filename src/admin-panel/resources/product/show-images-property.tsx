dfe56793974781043f763e4a07bbee3693de0dab
andrianka <andrianka13@gmail.coms>
2020-12-02 08:40:19 +0100

    change showImageProperty to oneline if

--- src/admin-panel/resources/product/show-images-property.tsx
+++ src/admin-panel/resources/product/show-images-property.tsx
@@ -9,11 +9,9 @@ const ShowImageProperty = ({ record }) => {
   for (let i = 0; i < 20; i++) {
     const path = '/images/' + record.params[`images.s3Key.${i}`];
     const filename = record.params[`images.filename.${i}`];
-    if (record.params[`images.s3Key.${i}`] !== undefined) {
-      images.push({ path: path, filename: filename });
-    } else {
-      break;
-    }
+    if (record.params[`images.s3Key.${i}`] == undefined) break;
+
+    images.push({ path: path, filename: filename });
   }
 
   return (
